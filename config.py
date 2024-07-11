09cf0abdd9ffa86e21d7f90e68a7b7b710c9b732
Виктор Бугай <ascon2913@gmail.com>
2024-07-12 04:08:21 +0500

    main and services done

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ ROOT_DIR = os.path.dirname(__file__)
 
 LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+LOGS_UTILS_DIR = os.path.join(LOGS_DIR, "utils.log")
+LOGS_VIEWS_DIR = os.path.join(LOGS_DIR, "views.log")
+LOGS_SERVICES_DIR = os.path.join(LOGS_DIR, "services.log")
--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,42 @@
-import json
-import os
 import datetime
+import json
+import logging
+import os
 from collections import defaultdict
 
-from src.excel_reader import read_excel_to_dataframe
-from config import ROOT_DIR, DATA_DIR
+import pandas as pd
 import requests
 from dotenv import load_dotenv
 
+from config import DATA_DIR, LOGS_UTILS_DIR, ROOT_DIR
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(LOGS_UTILS_DIR, "w")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s -%(funcName)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 
 def get_time_now():
-    '''
+    """
     функция для определения текущего времени
     :return: время в формате %Y-%m-%d %H:%M:%S
-    '''
+    """
     time_now = datetime.datetime.now()
     current_time = time_now.strftime("%Y-%m-%d %H:%M:%S")
     return current_time
 
 
-def greetings(time=get_time_now()):
-    '''
+def greetings(time):
+    """
     функция приветствует пользователя в зависимости от текущего времени
-    :param time: текущее время от get_time_now()
+    :param time: строка с текущим временем
     :return: словарь
-    '''
+    """
     time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
     hours = time.hour
+    logger.info("Функция начала обработку времени")
     if 6 <= hours < 12:
         greeting = "Доброе утро"
     elif 12 <= hours < 18:
@@ -35,130 +45,166 @@ def greetings(time=get_time_now()):
         greeting = "Добрый вечер"
     else:
         greeting = "Добрая ночь"
+    logger.info("Функция завершила работу и записала приветствие в словарь")
     greeting_time = {"greeting": greeting}
     return greeting_time
 
 
-def filtering_operations_by_date(operations=read_excel_to_dataframe(DATA_DIR + "/operations.xls"), date_now='2021-11-12 12:36:09'):
-    '''
-    функция фильтрует операции по текущей дате, так же отсеивает операции без даты платежа и если операция не прошла
-    :param date_now: текущая дата и время
-    :param operations: DataFrame
-    :return: отфильтрованный список словарей
-    '''
+def read_excel_to_list():
+    """
+    конвертирует excel файл в список
+    :return: List
+    """
+    logging.info("Функция начала работу")
+    try:
+        excel_file = DATA_DIR + "/operations.xls"
+        read = pd.read_excel(excel_file).to_json(orient="records")
+        result = json.loads(read)
+        return result
+    except FileNotFoundError:
+        logger.error("Произошла ошибка. Файл не найден")
+
+
+def filtering_operations_by_date(operations_data, date_now):
+    """
+    функция фильтрует операции по текущей дате,
+    так же отсеивает операции без даты платежа и если операция не прошла (FAILED)
+    :param operations_data: список операций
+    :param date_now: строка с датой
+    :return: отфильтрованный список операций
+    """
+    logger.info("Функция начала работу")
     date_now = datetime.datetime.strptime(date_now, "%Y-%m-%d %H:%M:%S")
-    operations_data = operations.to_json(orient="records")
-    data_py = json.loads(operations_data)
     filter_by_date = []
-    for data in data_py:
-        if data['Дата платежа'] and data['Статус'] == 'OK':
-            date_payment = datetime.datetime.strptime(data['Дата платежа'], '%d.%m.%Y')
-            if date_now.year == date_payment.year and date_now.month == date_payment.month and date_now.day >= date_payment.day:
-                filter_by_date.append(data)
+    try:
+        for data in operations_data:
+            if data["Дата платежа"] and data["Статус"] == "OK":
+                date_payment = datetime.datetime.strptime(data["Дата платежа"], "%d.%m.%Y")
+                if (
+                    date_now.year == date_payment.year
+                    and date_now.month == date_payment.month
+                    and date_now.day >= date_payment.day
+                ):
+                    filter_by_date.append(data)
+        logger.info("Функция успешно завершила работу")
+    except Exception as e:
+        logger.error(f"Произошла ошибка {e}")
+
     return filter_by_date
 
 
-def get_operations():
-    '''
-    функция использует отфильтрованный список по дате filtering_operations_by_date() и возвращает По каждой карте:
+def get_operations(operations):
+    """
+    функция использует отфильтрованный список по дате и возвращает По каждой карте:
             - последние 4 цифры карты;
             - общая сумма расходов;
             - кешбэк (1 рубль на каждые 100 рублей)
+    :param operations: отфильтрованный список словарей
     :return: список словарей
-    '''
-    operations = filtering_operations_by_date()
-    card_numbers = list(set(i['Номер карты'] for i in operations if i['Номер карты'] is not None))
+    """
+    logger.info("Функция начала работу")
+    card_numbers = list(set(i["Номер карты"] for i in operations if i["Номер карты"] is not None))
     payments = defaultdict(int)
+    logger.info("Функция обрабатывает данные")
     for card_number in card_numbers:
         for operation in operations:
-            if operation['Номер карты'] == card_number and operation['Сумма платежа'] < 0:
-                payments[card_number] += round(operation['Сумма платежа'])
+            if operation["Номер карты"] == card_number and operation["Сумма платежа"] < 0:
+                payments[card_number] += round(operation["Сумма платежа"])
 
     result = []
+    logger.info("Функция начинает формировать результат в список")
     for payment in payments:
-        result.append({"last_digits": payment[1:],
-                       "total_spent": payments[payment],
-                       "cashback": abs(payments[payment]) / 100})
+        result.append(
+            {"last_digits": payment[1:], "total_spent": payments[payment], "cashback": abs(payments[payment]) / 100}
+        )
+    logger.info("Функция успешно завершила работу")
     return result
 
 
-def top_five_operations():
-    '''
-    функция использует отфильтрованный список по дате filtering_operations_by_date()
+def top_five_operations(operations):
+    """
+    функция использует отфильтрованный список по дате
     возвращает топ-5 транзакций по сумме платежа
+    :param operations: отфильтрованный список словарей
     :return: список словарей
-    '''
-    operations = filtering_operations_by_date()
-    sorted_operations = sorted(operations, key=lambda x: x["Сумма операции"], reverse=True)[0:5]
+    """
+    logger.info("Функция начала работу. Идет сортировка")
+    sorted_operations = sorted(operations, key=lambda x: abs(x["Сумма платежа"]), reverse=True)[0:5]
     result = []
+    logger.info("Функция начинает формировать результат в список")
     for operation in sorted_operations:
-        result.append({"date": operation["Дата платежа"],
-                       "amount": operation["Сумма платежа"],
-                       "category": operation["Категория"],
-                       "description": operation["Описание"]})
+        result.append(
+            {
+                "date": operation["Дата платежа"],
+                "amount": operation["Сумма платежа"],
+                "category": operation["Категория"],
+                "description": operation["Описание"],
+            }
+        )
+    logger.info("Функция успешно завершила работу")
     return result
 
 
-def get_user_settings(settings=ROOT_DIR + '/user_settings.json'):
-    '''
+def get_user_settings():
+    """
     конвертирует json-объект в python
-    :param settings: путь до файла валютой и акциями
     :return: словарь
-    '''
-    with open(settings, "r", encoding="utf-8") as file:
-        user_settings = json.load(file)
-        return user_settings
+    """
+    settings = ROOT_DIR + "/user_settings.json"
+    logger.info("Функция начала работу. Идет открытие json-файла")
+    try:
+        with open(settings, "r", encoding="utf-8") as file:
+            user_settings = json.load(file)
+        logger.info("Функция успешно завершила работу")
+    except Exception as e:
+        logger.error(f"Произошла ошибка {e}")
+    return user_settings["user_currencies"], user_settings["user_stocks"]
 
 
-def get_currencies(user_currencies=get_user_settings()):
-    '''
-    принимает настройки пользователя в виде python-объекта
+def get_currencies(user_currencies):
+    """
+    использует настройки пользователя
     возвращает курсы валют
-    :param user_currencies: словарь с курсами валют
+    :param: список интересующих валют
     :return: список курса валют полученный через API
-    '''
-    load_dotenv()
-    url = "https://api.apilayer.com/exchangerates_data/latest"
-    API_KEY = os.getenv("API_KEY_CURS")
-    headers = {"apikey": API_KEY}
-    result = []
+    """
+    logger.info("Функция начала работу")
+    try:
+        load_dotenv()
+        url = "https://api.apilayer.com/exchangerates_data/latest"
+        API_KEY = os.getenv("API_KEY_CURS")
+        headers = {"apikey": API_KEY}
+        result = []
+        logger.info("Функция обращается к стороннему сервису для получения курса валют")
+        for i in user_currencies:
+            params = {"base": i, "sumbols": "RUB"}
+            response = requests.get(url, headers=headers, params=params)
+            response_result = response.json()
+            result.append({"currency": i, "rate": round(response_result["rates"]["RUB"], 2)})
+        logger.info("Функция успешно завершила работу")
+        return result
+    except Exception as e:
+        logger.error(f"Произошла ошибка {e}")
 
-    for i in user_currencies['user_currencies']:
-        params = {"base": i, "sumbols": "RUB"}
-        response = requests.get(url, headers=headers, params=params)
-        response_result = response.json()
-        result.append({"currency": i, "rate": response_result["rates"]["RUB"]})
 
-    return result
-
-
-def get_stocks(user_stocks=get_user_settings()):
-    '''
-    принимает настройки пользователя в виде python-объекта
-    :param user_stocks: словарь с акции
+def get_stocks(user_stocks):
+    """
+    использует настройки пользователя
+    :param: список интересующих акций
     :return: список цен на акции полученные через API
-    '''
-    load_dotenv()
-    API_KEY = os.getenv("API_KEY_STOCKS")
-    result = []
-
-    for i in user_stocks['user_stocks']:
-        url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={i}&apikey={API_KEY}'
-        response = requests.get(url)
-        response_result = response.json()
-        result.append({'stock': i, 'price': response_result['Global Quote']['05. price']})
-
-    return result
-
-
-# if __name__ == '__main__':
-
-
-    # print(get_stocks(get_user_settings()))
-    # print(get_currencies(get_user_settings()))
-    # print(get_user_settings())
-    # print(top_five_operations())
-    # print(greetings(get_time_now()))
-    # print(get_operations())
-    # print(get_user_settings(ROOT_DIR + '/user_settings.json'))
-    # print(get_currencies(get_user_settings))
+    """
+    logger.info("Функция начала работу")
+    try:
+        load_dotenv()
+        API_KEY = os.getenv("API_KEY_STOCKS")
+        result = []
+        logger.info("Функция обращается к стороннему сервису для получения цен на акции")
+        for i in user_stocks:
+            url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={i}&apikey={API_KEY}"
+            response = requests.get(url)
+            response_result = response.json()
+            result.append({"stock": i, "price": round(float(response_result["Global Quote"]["05. price"]), 2)})
+        logger.info("Функция успешно завершила работу")
+        return result
+    except Exception as e:
+        logger.error(f"Произошла ошибка {e}")
